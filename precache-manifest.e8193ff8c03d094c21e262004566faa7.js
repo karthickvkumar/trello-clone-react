a580b8a05bc9fb5d00123bcb8c28a296c3873d9c
kvelraj_concord <kvelraj@concord.net>
2020-10-12 12:18:56 +0530

    first commit

---
+++ precache-manifest.e8193ff8c03d094c21e262004566faa7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f853727af0e2b27f50c5",
+    "url": "/static/css/main.fa34fc61.chunk.css"
+  },
+  {
+    "revision": "f853727af0e2b27f50c5",
+    "url": "/static/js/main.4557c0ed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b29b34f22d2d247c92b5",
+    "url": "/static/js/2.7335b79d.chunk.js"
+  },
+  {
+    "revision": "31c682f8e0a96af0a11a3d229048dc44",
+    "url": "/index.html"
+  }
+];
